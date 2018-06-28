bd2130b6eebfed8e6ce5f437689246640c7cca21
nori007 <nori007@naver.com>
2018-06-28 17:40:37 +0900

    IE에서 안돌아가는 문법들 수정 / ES6문법 제거 / babel로 해도 되지만. 안쓰는 프로젝트가 있을수 있으니깐..

--- src/patternlock.js
+++ src/patternlock.js
@@ -1,14 +1,15 @@
 function PatternLock(options) {
-        // test
-    let mode = true;
-    let patternEvent = {}
+    var patternEvent = {};
+    var eventHandlers = {};
+    var mode = (parent.innerWidth < parent.yt.vikie.getConfig().mobileMaxWidth) ? true : false;
+    
     if(mode) {
         // mobile
         patternEvent.start = 'touchstart';
         patternEvent.move = 'touchmove';
         patternEvent.end = 'touchend';
     }else {
-        // pc
+        // pc v
         patternEvent.start = 'mousedown';
         patternEvent.move = 'mousemove';
         patternEvent.end = 'mouseup';
@@ -19,7 +20,8 @@ function PatternLock(options) {
         39: true, // right
         40: true, // down
         32: true, // spacebar
-        38: true, // pageup
+        // 38: true, // pageup
+        33: true, // pageup
         34: true, // pagedown
         35: true, // end
         36: true, // home
@@ -32,24 +34,24 @@ function PatternLock(options) {
         }
     }
     
-    let svgNS = 'http://www.w3.org/2000/svg';
-    let svgElement = document.createElementNS(svgNS, 'svg');
+    var svgNS = 'http://www.w3.org/2000/svg';
+    var svgElement = document.createElementNS(svgNS, 'svg');
     svgElement.setAttribute('class', 'patternlock');
     svgElement.setAttribute('id', 'lock');
     svgElement.setAttribute('viewBox', '0 0 100 100');
     svgElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
 
-    let activesElement = document.createElementNS(svgNS, 'g');
+    var activesElement = document.createElementNS(svgNS, 'g');
     activesElement.setAttribute('class', 'lock-actives');
-    let linesElement = document.createElementNS(svgNS, 'g');
+    var linesElement = document.createElementNS(svgNS, 'g');
     linesElement.setAttribute('class', 'lock-lines');
 
-    let dotsElement = document.createElementNS(svgNS, 'g');
+    var dotsElement = document.createElementNS(svgNS, 'g');
     dotsElement.setAttribute('class', 'lock-dots');
     
-    for(let i = 0; i < 3; i++){
-        for (let j = 0; j < 3; j++){
-            let circleElement = document.createElementNS(svgNS, 'circle');
+    for(var i = 0; i < 3; i++){
+        for (var j = 0; j < 3; j++){
+            var circleElement = document.createElementNS(svgNS, 'circle');
             circleElement.setAttribute('cx', 20 + (j * 30));
             circleElement.setAttribute('cy', 20 + (i * 30));
             circleElement.setAttribute('r', 2);
@@ -57,26 +59,24 @@ function PatternLock(options) {
         }
     }
 
-    let subSvgElement = document.createElementNS(svgNS, 'svg');
+    var subSvgElement = document.createElementNS(svgNS, 'svg');
 
     svgElement.appendChild(activesElement);
     svgElement.appendChild(linesElement);
     svgElement.appendChild(dotsElement);
     svgElement.appendChild(subSvgElement);
 
-    document.getElementById('testDiv').appendChild(svgElement);
-
-    let svg = svgElement
-    let self = this;
-    let dots = dotsElement.childNodes
-    let lines = linesElement
-    let actives = activesElement
+    var svg = svgElement
+    var self = this;
+    var dots = dotsElement.childNodes
+    var lines = linesElement
+    var actives = activesElement
     var pt = svg.createSVGPoint();
-    let code = []
-    let currentline
-    let currenthandler
+    var code = []
+    var currentline
+    var currenthandler
 
-    svg.addEventListener(patternEvent.start, (e)=> {
+    svg.addEventListener(patternEvent.start, function(e){
         clear();
         e.preventDefault();
         disableScroll();
@@ -84,34 +84,70 @@ function PatternLock(options) {
         
     });
     
-    svg.addEventListener(patternEvent.end, (e)=>{
+    svg.addEventListener(patternEvent.end, function(e){
         // 이벤트 한번만
-        document.removeEventListener(patternEvent.end, arguments.callee, false);
         end();
 
     })
 
+    function childAllRemove(element) {
+        while(element.hasChildNodes()) {
+            element.removeChild(element.firstChild);
+        }
+    }
+
+    function addClassName(element, className) {
+        if (element.classList) {
+            element.classList.add(className);
+        } else if (!(element.getAttribute('class').indexOf(className) > -1)) {
+            element.setAttribute('class', element.getAttribute('class') + ' ' + className);
+        }
+    }
+
+    function removeClassName(element, className) {
+        if (element.classList) {
+            element.classList.remove(className);
+        } else if (element.getAttribute('class').indexOf(className) > -1) {
+            element.setAttribute('class', element.getAttribute('class').replace(className, ' '));
+        }
+    }
+
+    function removeElement(element) {
+        var parent = element.parentNode;
+        parent.removeChild(element);
+    }
+    
     function success() {
-        svg.classList.remove('error');
-        svg.classList.add('success');
+        removeClassName(svg, 'error');
+        addClassName(svg, 'success');
     }
 
     function error() {
-        svg.classList.remove('success');
-        svg.classList.add('error');
+        removeClassName(svg, 'success');
+        addClassName(svg, 'error');
+    }
+
+    function clear() {
+        code = []
+        currentline = undefined;
+        currenthandler = undefined;
+        console.log(svg.className);
+        removeClassName(svg, 'success');
+        removeClassName(svg, 'error');
+        childAllRemove(lines);
+        childAllRemove(actives);
     }
 
     function getPattern() {
-        //return parseInt(code.map((i) => dots.index(i)+1).join(''))
-        let res = '', val = 0;
-        code.map((i) => {
+        var res = '', val = 0;
+        code.map(function(i){
             val = 0; 
-            dots.forEach((element, index) => {
-                if(element == i) {
-                    val = index + 1;
+            for(var j = 0; j < dots.length; j++) {
+                if(dots[j] == i) {
+                    val = j + 1;
                     res += val + '';
-                } 
-            });
+                }
+            }
         })
 
         return res;
@@ -120,31 +156,21 @@ function PatternLock(options) {
     function end() {
         enableScroll()
         stopTrack(currentline)
-        currentline && currentline.remove()
+        //currentline && currentline.remove()
+        removeElement(currentline);
         svg.removeEventListener(patternEvent.move, discoverDot);
-        let val = getPattern();
+        var val = getPattern();
         if(val == '' || val == undefined) {
             error()
         }else {
             success()
         }
-    }
-    
-    function childAllRemove(element) {
-        while(element.hasChildNodes()) {
-            element.removeChild(element.firstChild);
+
+        if(typeof eventHandlers['done'] === 'function') {
+            eventHandlers['done'](val);
         }
     }
     
-    function clear() {
-        code = []
-        currentline = undefined
-        currenthandler = undefined
-        svg.classList.remove('success');
-        svg.classList.remove('error');
-        childAllRemove(lines);
-        childAllRemove(actives);
-    }
 
     function preventDefault(e) {
         e = e || window.event;
@@ -179,7 +205,7 @@ function PatternLock(options) {
     }
 
     function isUsed(target) {
-        for (let i = 0; i < code.length; i++) {
+        for (var i = 0; i < code.length; i++) {
             if (code[i] === target) {
                 return true
             }
@@ -188,7 +214,7 @@ function PatternLock(options) {
     }
 
     function isAvailable(target) {
-        for (let i = 0; i < dots.length; i++) {
+        for (var i = 0; i < dots.length; i++) {
             if (dots[i] === target) {
                 return true
             }
@@ -200,7 +226,7 @@ function PatternLock(options) {
         return function(e) {
             e.preventDefault()
             if (currentline !== line) return
-            let pos = svgPosition(e.target, e)
+            var pos = svgPosition(e.target, e)
             line.setAttribute('x2', pos.x)
             line.setAttribute('y2', pos.y)
             return false
@@ -209,11 +235,11 @@ function PatternLock(options) {
 
     function discoverDot(e, target) {
         if (!target) {
-            let {x, y} = getMousePos(e)
-            target = document.elementFromPoint(x, y);
+            var location = getMousePos(e)
+            target = document.elementFromPoint(location.x, location.y);
         }
-        let cx = target.getAttribute('cx')
-        let cy = target.getAttribute('cy')
+        var cx = target.getAttribute('cx')
+        var cy = target.getAttribute('cy')
         if (isAvailable(target) && !isUsed(target)) {
             stopTrack(currentline, target)
             currentline = beginTrack(target)
@@ -226,16 +252,16 @@ function PatternLock(options) {
             svg.removeEventListener(patternEvent.move, currenthandler);
         }
         if (target === undefined) return
-        let x = target.getAttribute('cx')
-        let y = target.getAttribute('cy')
+        var x = target.getAttribute('cx')
+        var y = target.getAttribute('cy')
         line.setAttribute('x2', x)
         line.setAttribute('y2', y)
     }
 
     function beginTrack(target) {
         code.push(target)
-        let x = target.getAttribute('cx')
-        let y = target.getAttribute('cy')
+        var x = target.getAttribute('cx')
+        var y = target.getAttribute('cy')
         var line = createNewLine(x, y)
         var marker = createNewMarker(x, y)
         actives.appendChild(marker)
@@ -269,27 +295,28 @@ function PatternLock(options) {
     }
 
     function getMousePos(e) {
-        if(mode) {
-            return {
-                x: e.clientX || e.touches[0].clientX,
-                y :e.clientY || e.touches[0].clientY
-            }    
-        }else {
-            return {
-                x: e.clientX || e.originalEvent.touches[0].clientX,
-                y :e.clientY || e.originalEvent.touches[0].clientY
-            }
+        return {
+            x: e.clientX || ((e.originalEvent) ? e.originalEvent.touches[0].clientX : e.touches[0].clientX),
+            y: e.clientY || ((e.originalEvent) ? e.originalEvent.touches[0].clientY : e.touches[0].clientY)
         }
     }
 
     function svgPosition(element, e) {
-        let {x, y} = getMousePos(e)
-        pt.x = x; pt.y = y;
+        var location = getMousePos(e)
+        pt.x = location.x; pt.y = location.y;
         return pt.matrixTransform(element.getScreenCTM().inverse());
     }
 
     return {
-        getSVG: function() {return svg},
-        getPattern: getPattern
+        getSVG: function() { return svg },
+        getPattern: getPattern,
+        addEventListener: function(name, handler) {
+            switch(name) {
+                case 'done':
+                if(eventHandlers.hasOwnProperty(name)) return;
+                eventHandlers[name] = handler;
+                break;
+            }
+        }
     }
 };
